2feaf31c59e49dd25b5ddb4dbfeec012eaf86058
Kimjingook <wlsrnrsla@gmail.com>
2014-05-20 22:15:17 +0900

    GameView 비율 구현

--- DdongGame/DdongGame/src/kr/co/ddonggame/game/GameView.java
+++ DdongGame/DdongGame/src/kr/co/ddonggame/game/GameView.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.View;
 
 public class GameView extends View {
@@ -16,8 +17,22 @@ public class GameView extends View {
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
+		int x,y;
+		x=canvas.getWidth();
+		y=canvas.getHeight();
+		x/=4;
 		Bitmap card = BitmapFactory.decodeResource(getResources(), R.drawable.card_back);
-		canvas.drawBitmap(card, 0, 0, null);
+		Bitmap card2 = BitmapFactory.decodeResource(getResources(), R.drawable.card_back);
+		Bitmap card3 = BitmapFactory.decodeResource(getResources(), R.drawable.card_back);
+		Bitmap card4 = BitmapFactory.decodeResource(getResources(), R.drawable.card_back);
+		/*canvas.drawBitmap(card, 0, 0, null);
+		canvas.drawBitmap(card2, x, 0, null);
+		canvas.drawBitmap(card3, 2*x, 0, null);
+		canvas.drawBitmap(card4, 3*x, 0, null);*/
+		canvas.drawBitmap(card,null,new Rect(0,0,x,y),null);
+		canvas.drawBitmap(card2,null,new Rect(x,0,2*x,y),null);
+		canvas.drawBitmap(card3,null,new Rect(2*x,0,3*x,y),null);
+		canvas.drawBitmap(card4,null,new Rect(3*x,0,4*x,y),null);
 		super.onDraw(canvas);
 	}
 
