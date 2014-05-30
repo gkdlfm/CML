c3ed36bd9e677bb2bbadc1675459279c4b56bca0
parkkwangyoul <rhkdduf63@gmail.com>
2014-05-30 14:57:20 +0900

    btnCreate 추가

--- DdongGame/DdongGame/src/kr/co/ddonggame/GameRoom.java
+++ DdongGame/DdongGame/src/kr/co/ddonggame/GameRoom.java
@@ -20,60 +20,73 @@ public class GameRoom extends ActionBarActivity implements OnClickListener {
 	private Button[] btnRoom;
 	private TextView[] roomTextView;
 	boolean[] roomOpenAndClose;
+	
 	private ClientThread clientThread;
+	
+	private Button btnCreate;
 	private Button btnLeft;
 	private Button btnRight;
+	
 	private int roomList = 1;
-	private int roomEnterNumber=0;
+	private int roomEnterNumber = 0;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_gameroom);
+		
+		btnCreate = (Button) findViewById(R.id.btnCreate);
+
 		btnRoom = new Button[6];
 		roomTextView = new TextView[6];
 		roomOpenAndClose = new boolean[6];
-		btnLeft = (Button)findViewById(R.id.btnRoomLeft);
+		btnLeft = (Button) findViewById(R.id.btnRoomLeft);
 		btnLeft.setOnClickListener(this);
-		btnRight = (Button)findViewById(R.id.btnRoomRight);
+		btnRight = (Button) findViewById(R.id.btnRoomRight);
 		btnRight.setOnClickListener(this);
+		
 		for (int i = 1; i <= 6; i++) {
 			int temp = getResources().getIdentifier("btnRoom" + i + "Enter",
 					"id", "com.example.ddonggame");
 			btnRoom[i - 1] = (Button) findViewById(temp);
 			btnRoom[i - 1].setOnClickListener(this);
-			int temp2 = getResources().getIdentifier("room"+i+"TextView", "id", "com.example.ddonggame");
-			roomTextView[i-1] = (TextView)findViewById(temp2);
+			int temp2 = getResources().getIdentifier("room" + i + "TextView",
+					"id", "com.example.ddonggame");
+			roomTextView[i - 1] = (TextView) findViewById(temp2);
 		}
 
 		clientThread = ClientThread.getInstance();
 		clientThread.getRoomList(roomList);
 		clientThread.getClient().setGameRoom(this);
-		//GameRoom Activity요청시 서버에서 방목록의 정보를 가져와서 room1TextView의 text (방 번호)를 바꾸어준다.
+		// GameRoom Activity요청시 서버에서 방목록의 정보를 가져와서 room1TextView의 text (방 번호)를
+		// 바꾸어준다.
 	}
 
-	public void changeRoomInformation(int gameRoomNumber, String roomOpenOrClose){
+	public void changeRoomInformation(int gameRoomNumber, String roomOpenOrClose) {
 		Log.i("gameRoomNumber", Integer.toString(gameRoomNumber));
-		Log.i("roomopenorclose",roomOpenOrClose);
-		for(int i=1; i<=6; i++){
-			int temp = (gameRoomNumber-1)*6+i;
-			roomTextView[i-1].setText(Integer.toString(temp));
-			char a = roomOpenOrClose.charAt(i-1);
-			if(a=='0'){
-				btnRoom[i-1].setEnabled(false);
-			}
-			else{
-				btnRoom[i-1].setEnabled(true);
+		Log.i("roomopenorclose", roomOpenOrClose);
+		
+		for (int i = 1; i <= 6; i++) {
+			int temp = (gameRoomNumber - 1) * 6 + i;
+			roomTextView[i - 1].setText(Integer.toString(temp));
+			char a = roomOpenOrClose.charAt(i - 1);
+			if (a == '0') {
+				btnRoom[i - 1].setEnabled(false);
+			} else {
+				btnRoom[i - 1].setEnabled(true);
 			}
 		}
-		
+
 	}
-	
+
 	public void onClick(View v) {
 		int id = v.getId();
-		int roomNumber=0;
+		int roomNumber = 0;
 		switch (id) {
+		case R.id.btnCreate:
+			clientThread
 		case R.id.btnRoom1Enter:
 			roomNumber = Integer.parseInt(roomTextView[0].getText().toString());
 			clientThread.getRoomEnter(roomNumber);
@@ -110,9 +123,10 @@ public class GameRoom extends ActionBarActivity implements OnClickListener {
 			break;
 		}
 	}
-	public void roomEnter(int roomEnterNumber){
+
+	public void roomEnter(int roomEnterNumber) {
 		this.roomEnterNumber = roomEnterNumber;
 		startActivity(new Intent(this, RoomEnter.class));
 	}
-	
+
 }
