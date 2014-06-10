e4e6082108a7bfde11ff41323788fc4023a37480
Kimjingook <wlsrnrsla@gmail.com>
2014-06-11 02:19:51 +0900

    진짜 끝~

--- DdongGame/DdongGame/src/com/example/ddonggame/OptionActivity.java
+++ DdongGame/DdongGame/src/com/example/ddonggame/OptionActivity.java
@@ -2,16 +2,18 @@ package com.example.ddonggame;
 
 import kr.co.ddonggame.client.ClientThread;
 import kr.co.ddonggame.client.UserInformation;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.app.Activity;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class OptionActivity extends Activity implements OnClickListener,
 		OnCheckedChangeListener {
@@ -20,6 +22,8 @@ public class OptionActivity extends Activity implements OnClickListener,
 	private RadioButton radioTypeA;
 	private RadioButton radioTypeB;
 	private RadioButton radioTypeC;
+	private Button btnOK;
+	private Button btnNO;
 	private UserInformation userInformation = UserInformation.getInstance();
 	private ImageView typeImage;
 	private BitmapDrawable typeImageDrawable;
@@ -32,31 +36,51 @@ public class OptionActivity extends Activity implements OnClickListener,
 
 		radioType = (RadioGroup) findViewById(R.id.radio_type);
 		radioTypeA = (RadioButton) findViewById(R.id.radio_type_a);
+		radioTypeA.setId(1);
 		radioTypeB = (RadioButton) findViewById(R.id.radio_type_b);
+		radioTypeB.setId(2);
 		radioTypeC = (RadioButton) findViewById(R.id.radio_type_c);
-
+		radioTypeC.setId(3);
+		btnOK = (Button)findViewById(R.id.option_yes_btn);
+		btnNO = (Button)findViewById(R.id.option_no_btn);
+		btnOK.setOnClickListener(this);
+		btnNO.setOnClickListener(this);
 		radioType.setOnCheckedChangeListener(this);
-
 		typeImage = (ImageView) findViewById(R.id.type_image);
+		String type = userInformation.getType();
+		if(type.equals("a")){
+			radioType.check(1);
+			//onCheckedChanged(radioType, 1);
+		}else if(type.equals("b")){
+			radioType.check(2);
+			//onCheckedChanged(radioType, 2);
+		}
+		else if(type.equals("c")){
+			radioType.check(3);
+			//onCheckedChanged(radioType, 3);
+		}
 	}
 
 	@Override
 	public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
 		switch (checkedId) {
-		case R.id.radio_type_a:
+		case 1:
 			typeImageDrawable = (BitmapDrawable) getResources().getDrawable(
 					R.drawable.a1_1);
-			
+			clientThread.typeSet("a");
+			userInformation.setType("a");
 			break;
-		case R.id.radio_type_b:
+		case 2:
 			typeImageDrawable = (BitmapDrawable) getResources().getDrawable(
 					R.drawable.b1_4);
-
+			clientThread.typeSet("b");
+			userInformation.setType("b");
 			break;
-		case R.id.radio_type_c:
+		case 3:
 			typeImageDrawable = (BitmapDrawable) getResources().getDrawable(
 					R.drawable.c1_4);
-
+			clientThread.typeSet("c");
+			userInformation.setType("c");
 			break;
 		}
 
@@ -73,12 +97,19 @@ public class OptionActivity extends Activity implements OnClickListener,
 			userInformation.setType("a");
 			break;
 		case R.id.radio_type_b:
+			Log.i("test", "bbb");
 			clientThread.typeSet("b");
 			userInformation.setType("b");
 			break;
 		case R.id.radio_type_c:
 			clientThread.typeSet("c");
 			userInformation.setType("c");
+			break;
+		case R.id.option_yes_btn:
+			finish();
+			break;
+		case R.id.option_no_btn:
+			finish();
 			break;
 		}
 	}
